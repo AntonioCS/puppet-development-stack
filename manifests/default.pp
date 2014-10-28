46bac4c0df87219d15a4d65e044eac9e0e3e5285
antoniocs <antoniocs@gmail.com>
2014-10-28 23:35:21 +0000

    Calling a few more modules

--- manifests/default.pp
+++ manifests/default.pp
@@ -5,9 +5,33 @@ Exec {
 
 include bootstrap
 include tools
+
 include apache
-include php
-include php::pear
-include php::pecl
-include mysql
+apache::mod { 'rewrite': }
+
+#include php::cli 
+#include php::mod_php5
+
+#php::module { 'xdebug':}
+
+
+include mysql::server
+include gcc
+
+package { 'gdb':
+	ensure => 'present',
+	}
+
+include git
+git::config { 'user.name':
+  value => 'antoniocs',
+}
+
+git::config { 'user.email':
+  value => 'antoniocs@gmail.com',
+}
+
+package { 'puppet': 
+	ensure => 'latest',
+}
 
