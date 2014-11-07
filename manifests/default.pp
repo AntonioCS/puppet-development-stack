0c2c96a402f7ab2204636bc6af9073949db693ab
antoniocs <antoniocs@gmail.com>
2014-11-07 08:41:28 +0000

    Trying to set up config for php and mysql

--- manifests/default.pp
+++ manifests/default.pp
@@ -6,22 +6,45 @@ Exec {
 include bootstrap
 include tools
 
+
+#https://forge.puppetlabs.com/puppetlabs/apache
 include apache
-apache::mod { 'rewrite': }
-
-#include php::cli 
-#include php::mod_php5
-
-#php::module { 'xdebug':}
+apache::mod::rewrite
 
 
-include mysql::server
+#include '::mysql::server'
+
+
+#class { '::mysql::server':	  
+#	  'override_options' => { 
+#		'mysqld' => { 
+#			'max_connections' => '1024',
+#			'bind-address' => '0.0.0.0',
+#			'log-slow-queries' => '1',
+#			'slow-query-log-file' = '/var/log/mysql/slow_query.log',
+#		} 
+#	}
+#}
+
+#http://puppet-php.readthedocs.org/en/latest/index.html
+
+# Install extensions
+
+include php
+
+class { ['php::apache','php::cli','php::extension::curl','php::extension::imagick','php::extension::mcrypt','php::extension::memcached','php::extension::mysql','php::extension::xdebug','php::composer','php::composer::auto_update']: }
+class { 'php::phpunit':
+  ensure => latest,
+}
+
+
+
 include gcc
-
 package { 'gdb':
 	ensure => 'present',
 	}
 
+	
 include git
 git::config { 'user.name':
   value => 'antoniocs',
