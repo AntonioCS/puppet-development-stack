5b105be00809ccdfb9c32fead60ca634fb4ae15c
antoniocs <antoniocs@gmail.com>
2015-03-16 22:25:55 +0000

    Apache virtualhost. Mysql configs. PHP configs

--- manifests/default.pp
+++ manifests/default.pp
@@ -8,23 +8,37 @@ include tools
 
 
 #https://forge.puppetlabs.com/puppetlabs/apache
-include apache
-#apache::mod::rewrite
+#https://groups.google.com/forum/#!msg/puppet-users/Tem7vo-iX_A/ILgFsAP2Cb8J
+class { 'apache':
+	mpm_module => 'prefork',
+}
 
 
-#include '::mysql::server'
+
+class {'::apache::mod::php': }
+class {'::apache::mod::rewrite': }
+
+apache::vhost { 'irunerrands.dev':
+	port => '80',
+	docroot => '/home/vagrant/projects/Personal/php/irunerrands/public',
+	serveradmin => 'admin@admin.dev',
+	options => ['Indexes','FollowSymLinks','MultiViews'],
+	setenv => ["APP_ENV dev"],
+	override => ['All'],
+}
 
 
-#class { '::mysql::server':	  
-#	  'override_options' => { 
-#		'mysqld' => { 
-#			'max_connections' => '1024',
-#			'bind-address' => '0.0.0.0',
-#			'log-slow-queries' => '1',
-#			'slow-query-log-file' = '/var/log/mysql/slow_query.log',
-#		} 
-#	}
-#}
+
+class { '::mysql::server':
+	override_options => {
+		'mysqld' => {
+			'long_query_time' => 1,
+			'slow_query_log' => 1,
+			'slow_query_log_file' => '/var/log/mysql/mysql_slow_query',
+			'log' => '/var/log/mysql/mysql_all_queries'
+		}
+	}
+}
 
 #http://puppet-php.readthedocs.org/en/latest/index.html
 
@@ -32,27 +46,47 @@ include apache
 
 include php
 
+# Install extensions
+Php::Extension <| |>
+	# Configure extensions
+	-> Php::Config <| |>
+	# Reload webserver
+	~> Service["apache2"]
+
 class {
 	[
 		'php::apache',
 		'php::cli',
-		
+
 		'php::extension::curl',
 		'php::extension::imagick',
 		'php::extension::mcrypt',
 		'php::extension::memcached',
 		'php::extension::mysql',
 		'php::extension::xdebug',
-		
 		'php::composer',
 		'php::composer::auto_update'
 	]: 
 }
 
-#Error: Could not find resource 'Exec[php::pear::auto_discover]' for relationship on 'Package[pear.phpunit.de/PHPUnit]'
-#class { 'php::phpunit':
-#  ensure => latest,
-#}
+
+php::config { 'display_errors=On':
+	file => '/etc/php5/apache2/php.ini',
+	#section => 'PHP', #defaults to PHP
+}
+php::config {
+	[
+		'xdebug.remote_enable=1',
+		'xdebug.remote_host=192.168.56.1',
+		'xdebug.remote_port=9000',
+		'xdebug.remote_handler=dbgp'
+	]:
+	file => '/etc/php5/apache2/php.ini',
+	section => 'Xdebug',
+}
+
+include phpunit
+
 
 include gcc
 
@@ -60,17 +94,20 @@ package { 'gdb':
 	ensure => 'present',
 }
 
+
+#Installs really old version
 package { 'cmake':
-	ensure => 'present',
+	ensure => 'absent',
 }
 	
 include git
+
 git::config { 'user.name':
-  value => 'antoniocs',
+  value => 'antoniocs'
 }
 
 git::config { 'user.email':
-  value => 'antoniocs@gmail.com',
+  value => 'antoniocs@gmail.com'
 }
 
 package { 'puppet':
@@ -80,5 +117,11 @@ package { 'puppet':
 package { 'inoticoming':
 	ensure => 'present',
 }
+
+#package { 'clip':
+#	ensure => 'present',
+#}
+
+
 
 include '::rabbitmq'
